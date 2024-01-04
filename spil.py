e900ac007519cd9ecddad39a24af78eb7befa63e
Søren Magnusson <soren@magnussons.dk>
2024-01-04 13:06:50 +0100

    Spørger om man vil spille igen

--- spil.py
+++ spil.py
@@ -1,16 +1,18 @@
 import random
 
-tal = random.randint(1, 100)
-
-# print(tal)
-
 while True:
-    gæt = int(input("Hvad gætter du på? "))
-    # print(gæt)
-    if gæt == tal:
-        print("Ja, tallet er", gæt)
+    tal = random.randint(1, 100)
+    # print(tal)
+    while True:
+        gæt = int(input("Hvad gætter du på? "))
+        # print(gæt)
+        if gæt == tal:
+            print("Ja, tallet er", gæt)
+            break
+        elif tal < gæt:
+            print("Tallet er lavere end", gæt)
+        else:
+            print("Tallet er højere end", gæt)
+    svar = input("Vil du spille igen (J/n)")
+    if svar == "n":
         break
-    elif tal < gæt:
-        print("Tallet er lavere end", gæt)
-    else:
-        print("Tallet er højere end", gæt)
